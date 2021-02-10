604a7ee5c486897b83a3dca8dfef665ad839619b
goDlin <godlin086@gmail.com>
2021-02-10 18:10:34 +0300

    grid update

--- src.js
+++ src.js
@@ -26,37 +26,31 @@
 
         let bitcoin, ethereum, bitcoinYday, ethereumYday
 
-        datas.map(({ data }, i) => {
-            let id = data.base === 'BTC' ? 'bitcoin' : 'ethereum'
-            if (i > 1) id += 'Yday'
-            
-            document.getElementById(id).innerHTML = parseFloat(data.amount).toFixed(2).toString() + '$'
-
+        datas.map(({ data }, i) => {            
             if (i < 2) {
                 data.base === 'BTC' ? bitcoin = data.amount : ethereum = data.amount
+                document.getElementsByClassName('amount')[i].innerHTML = parseFloat(data.amount).toFixed(2).toString() + '$'
             } else {
                 data.base === 'BTC' ? bitcoinYday = parseFloat(data.amount).toFixed(2) : ethereumYday = parseFloat(data.amount).toFixed(2)
 
                 const sub = data.base === 'BTC' ? bitcoin - bitcoinYday : ethereum - ethereumYday
 
-                let html = '<span class="'
+                let html = '<div class="yday">' + parseFloat(data.amount).toFixed(2).toString() + '$<span class="'
                 html += sub > 0 ? 'plus">+' : 'minus">'
-                html += sub.toFixed(2).toString() + '</span>'
-                document.getElementById(id).innerHTML += html                
+                html += sub.toFixed(2).toString() + '</span></div>'
+                document.getElementsByClassName('amount')[i - coins.length].innerHTML += html                
             }
         })
 
         const r = bitcoin / ethereum
         const rYday = bitcoinYday / ethereumYday
-    
-        document.getElementById('btceth').innerText = r.toFixed(2).toString() + '/1'
-        document.getElementById('btcethYday').innerHTML = rYday.toFixed(2).toString() + '/1'
-
         const rh = r - rYday
-        let html = '<span class="'
+    
+        let html = r.toFixed(2).toString() + '/1 <div class="yday">' + rYday.toFixed(2).toString() + '/1'
+        html += '<span class="'
         html += rh > 0 ? 'plus">+' : 'minus">'
-        html += rh.toFixed(2).toString() + '/1</span>'
-        document.getElementById('btcethYday').innerHTML += html
+        html += rh.toFixed(2).toString() + '/1</span></div>'
+        document.getElementsByClassName('amount')[2].innerHTML = html
 
     } catch (error) {
         console.log(error)
